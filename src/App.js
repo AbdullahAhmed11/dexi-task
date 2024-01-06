cc857551bcd81d8cf711cf546d1ef19efaac5e07
Abdullahahmed11 <abdullah77zzx@gmail.com>
2024-01-06 16:27:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { useState, useEffect } from "react";
 import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
+
   Box,
-  Flex,
-  Image,
+
   Text,
-  Button,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
+
 } from '@chakra-ui/react'
 import PopUp from "./components/Modal";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
@@ -33,22 +16,6 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <BrowserRouter>
-          <ul className="nav">
-            <li>
-              <Link to="/one">Challenge 1</Link>
-            </li>
-            <li>
-              <Link to="/two">Challenge 2</Link>
-            </li>
-            <li>
-              <Link to="/three">Challenge 3</Link>
-            </li>
-          </ul>
-          <Routes />
-        </BrowserRouter>
-      </div>
 
       <Box>
         <Box alignItems="center" justifyContent="center" display="flex" flexDirection="column" gap="10px">
